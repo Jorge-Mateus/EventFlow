03afc6d7badf4ee93d8ec15990fc05c88b82a78d
Jorge Mateus <69761413+Jorge-Mateus@users.noreply.github.com>
2026-05-22 16:41:59 -0300

    Cliente controller e view

---
+++ EventFlow.Web/Controllers/ClienteController.cs
@@ -0,0 +1,42 @@
+﻿using EventFlow.Application.DTOs.Cliente;
+using EventFlow.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventFlow.Web.Controllers
+{
+    public class ClienteController : Controller
+    {
+        private readonly IClienteService _service;
+
+        public ClienteController(
+            IClienteService service)
+        {
+            _service = service;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var clientes =
+                await _service.ObterTodosAsync();
+
+            return View(clientes);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(
+            CriarClienteDto dto)
+        {
+            if (!ModelState.IsValid)
+                return View(dto);
+
+            await _service.CriarAsync(dto);
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
